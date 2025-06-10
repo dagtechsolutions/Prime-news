874a19f7d7ec512ce27146ff5834e04b39513ed3
Dawud <38299773+dagtechsolutions@users.noreply.github.com>
2025-06-10 16:47:38 +0100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    const mainNav = document.querySelector('.main-navigation');
 
+    if (navToggle) {
+        navToggle.addEventListener('click', () => {
+            mainNav.classList.toggle('is-active');
+            navToggle.classList.toggle('is-active');
+        });
+    }
+});
